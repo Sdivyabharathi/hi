122428205da6e07496e18b025440f1e767873654
Sdivyabharathi <divyaarul96@gmail.com>
2017-07-06 22:05:00 +0530

    Create Swapeven.java

---
+++ Swapeven.java
@@ -0,0 +1,29 @@
+# hi
+import java.util.*;
+import java.io.*; 
+public class Swapeven
+{
+  public static String swapPairs(String s) {
+    String even = "";
+    String odd = "";
+    int length = s.length();
+    
+    for (int i = 0; i <= length-2; i+=2) {          
+      even += s.charAt(i+1) + "" + s.charAt(i);
+    }
+    
+    if (length % 2 != 0) {          
+      odd = even + s.charAt(length-1);
+      return odd;
+    } else {
+      return even;
+    }
+  }
+  public static void main(String[] args)
+  {
+    Swapeven sc= new Swapeven();
+    System.out.println("given string(' system')");
+    System.out.println(sc.swapPairs("system"));
+    
+  }
+}
