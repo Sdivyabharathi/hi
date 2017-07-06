0b3cc206643b51c70b32d5cf226b3b4c50dfa467
Sdivyabharathi <divyaarul96@gmail.com>
2017-07-06 21:54:25 +0530

    Create Prime.java

---
+++ Prime.java
@@ -0,0 +1,27 @@
+# hi
+import java.io.*;
+import java.util.*;
+public class Prime
+{ 
+  public static void main(String args[]) 
+  { 
+    int num =1,b,c,range; 
+    Scanner s=new Scanner(System.in); 
+    System.out.println("Enter The Range \n"); 
+    range=s.nextInt(); 
+    while(num <=range) 
+    { 
+      b=1; 
+      c=0; 
+      while(b<= num) 
+      { 
+        if((num%b)==0) 
+        c=c+1; 
+        b++; 
+      } 
+      if(c==2) 
+      System.out.println(num); 
+      num ++; 
+    } 
+  } 
+} 
